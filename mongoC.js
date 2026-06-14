b0baaabb1ee45da25cf17ab65657e8f8f6a09c75
ahmed yassen <ahmedyassen.b7@gmail.com>
2026-06-14 15:14:18 +0300

    new cicd

--- mongoC.js
+++ mongoC.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@devcluster.xf2gcci.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+const connectionString = `mongodb+srv://ahmedyassenb7:${password}@testin-db.bqkimtd.mongodb.net/?appName=testin-db`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
